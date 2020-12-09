22eef693dda2dd53528c3cf832de0a623a6ac790
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-12-09 12:15:46 -0500

    wrote JS to append data to modal divs--modal still needs styled, there is a bug with the way the hours are displayed

--- assets/js/project.js
+++ assets/js/project.js
@@ -4,9 +4,7 @@
 //use that to identify the parks in the state
 //then rerun it to pull the data fields
 
-//create a function that allows me to fetch the data, loop through it
-//grab out the specific National Parks to put onhange the modal
-
+//function to collect user selected state and fetch parks in that state
 var parkOptions= function() {
   
 //set up variables that hold the state choices and values from the main drop down
@@ -29,12 +27,13 @@ fetch(url).then(function(response) {
        var parkCode=response.data[i].parkCode;
        $('#park').append(`<option value="${parkCode}"> ${parkName}</option>`); 
        console.log(parkName, parkCode);
-     // }
+       //append park name to modal title
+       $('.modal-card-title').html(parkName);
     }
   })
 };
-
- var getDataPoints= function() {
+//function to get park data from user selected park
+var getDataPoints= function() {
     var selectPark = document.getElementById("park");
     var chosenPark= selectPark.value;
   
@@ -47,27 +46,49 @@ fetch(url).then(function(response) {
         console.log(response.data[0]);
 
   //// Use the park code to get the data points
-  var hours = response.data[0].operatingHours;
-  var description= response.data[0].descripion;
-  var directionsInfo= response.data[0].directionsInfo;
-  var directionsUrl= response.data[0].directionsUrl;
-  //city to plug into the weather API
-  var city= response.data[0].addresses[0].city;
-  
-  console.log(hours, description, directionsInfo, directionsUrl, city);
-
-  //input data into modal divs
-  //put function to display weather data inside this function
-//handle bug for only one park in a state 
+    var hourDetails = response.data[0].operatingHours[0].description;
+    var hours = response.data[0].operatingHours[0].standardHours;
+    var formattedHours = JSON.stringify(hours);
+    var description= response.data[0].description;
+    var directionsInfo= response.data[0].directionsInfo;
+    var directionsUrl= response.data[0].directionsUrl;
+    //city to plug into the weather API
+    var city= response.data[0].addresses[0].city;
+    
+    console.log(hours, hourDetails, description, directionsInfo, directionsUrl, city);
+    $('#parkHours').html(`Hours: <br> ${formattedHours}`);
+    $('#description').html(`Description: <br> ${description}`);
+    $('#directions').html(`Directions: <br> ${directionsInfo} <br> <a href=${directionsUrl}>${directionsUrl}</a>`);
+    //input data into modal divs
+    //put function to display weather data inside this function
+    //handle bug for only one park in a state  
+    getCurrentWeather(city);
 })
+    //fetch data from API for current weather using city from park api data
+    var getCurrentWeather = function(city) {
+        //console.log(1);
+        var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=046dddbb0aa4d31febc4e77558997908";
+
+        fetch(apiUrl)
+        .then(function(response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    //console.log(data);
+                    displayCurrentWeather(data, city);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+    };
+    
 };
 
 //modal open on change of select menu
 $("#park").change(function() {
     $(".modal").addClass("is-active");
-    //when modal opens call functions to fetch and display weather data
-    getCurrentWeather();
-    displayCurrentWeather();
+    $('#park').html('');
 
 });
 //modal close
@@ -75,30 +96,15 @@ $("#modalClose").on("click", function() {
     $(".modal").removeClass("is-active");
 });
 
-//fetch data from API for current weather
-var getCurrentWeather = function(city) {
-    console.log(1);
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=046dddbb0aa4d31febc4e77558997908";
-
-    fetch(apiUrl)
-    .then(function(response) {
-        //request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-                displayCurrentWeather(data, city);
-            });
-        } else {
-            alert("Error: " + response.statusText);
-        }
-    })
-};
 //display current conditions
 var displayCurrentWeather = function(data) {
     var temp = data.main.temp;
     var humidity = data.main.humidity;
     var windSpeed = data.wind.speed;
     document.querySelector("#currentConditions").innerHTML = `
+        <br>
+        Current Weather Conditions:
+        <br>
         <p>Temperature: ${temp} °F<p>
         <p>Humidity: ${humidity} %<p>
         <p>Wind Speed: ${windSpeed} MPH<p>
