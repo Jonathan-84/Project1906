6379a3e50bef209b37d3fddfaf94519bd528c20b
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-12-10 01:05:43 -0500

    fixed he mobile readiness, added a little bit of CSS and also added screenshots for ReadMe which I wrote

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # Project1906
 Plan your visit to a National Park
 
+## Project's Aim
+Who wants to plan a trip to the a US National Park when it's miserable outside. Currently, you would need to use multiple resources to find out park information and the weather. This project aims to simplify that process and give you the information that you need. Information that includes:
+- Park Description
+- Current Weather
+- 5 day Forecast
+- Park Directions
+- URL to National Park Services Directions page for the park 
 
-++ Small change to add to computer and Github
+### Technologies 
+Leveraging Javascript, HTML, CSS, Bulma CSS Framework, JQuery, APIs (National Park Service API and Open Weather API)
+
+#### Functionalities
+Drop Downs enable you to select a state
+Fetching data from the National Park Service API we are able to populate the 2nd (Park Dropdown)
+Based on Park selection, we fetch the key data highlighted above as well as the Park's city
+The city is then used to extract the weather data from the Open Weather API
+This key information appears on the page via a modal
+We save your search to local storage for quick recall
+
+##### How-To Use
+This is a web based app. You can visit the page. You must start by selecting the state then select the specific park.
+
+![575 screen](insert mobile screenshot URL)
+
+![768 screen](insert tablet screen URL)
+
+![980 screen](insert desktop screen URL)
+
--- assets/js/project.js
+++ assets/js/project.js
@@ -4,56 +4,112 @@
 //use that to identify the parks in the state
 //then rerun it to pull the data fields
 
-//create a function that allows me to fetch the data, loop through it
-//grab out the specific National Parks to put onhange the modal
-
+//function to collect user selected state and fetch parks in that state
 var parkOptions= function() {
   
-//set up variables that hold the state choices and values from the main drop down
-var selectOptions = document.getElementById("state");
-var options= selectOptions.value;
-console.log(options);
-var url = 'https://developer.nps.gov/api/v1/parks?stateCode=' + options + '&api_key=yCwb05JC1ccmoZ4jFHTMcbiQIa3pV5MAeI22MlmG';
-
-fetch(url).then(function(response) {
-    // Pass the data from the first fetch
-    return response.json(); 
-  }).then(function(response) {
-    console.log(response.data);
-    //Make a variable of the value wanted from the first api call
-    for (var i = 0; i < response.data.length; i ++) {
-        // use jquery to append or add fullName to modal
-       var parkName= response.data[i].fullName;
-       // store park code or if possible assign it as a value to the fullName
-       //if so, I can then do a separate fetch on the click that uses the parkCode to extract needed data
-       var parkCode=response.data[i].parkCode;
-       $('#park').append(`<option value="${parkCode}"> ${parkName}</option>`); 
-       console.log(parkName, parkCode);
-     // }
-    }
+  //set up variables that hold the state choices and values from the main drop down
+  var selectOptions = document.getElementById("state");
+  var options= selectOptions.value;
+  console.log(options);
+  var url = 'https://developer.nps.gov/api/v1/parks?stateCode=' + options + '&api_key=yCwb05JC1ccmoZ4jFHTMcbiQIa3pV5MAeI22MlmG';
+  
+  fetch(url).then(function(response) {
+      // Pass the data from the first fetch
+      return response.json(); 
+    }).then(function(response) {
+      console.log(response.data);
+      //Make a variable of the value wanted from the first api call
+      for (var i = 0; i < response.data.length; i ++) {
+  
+          // use jquery to append or add fullName to modal
+         var parkName= response.data[i].fullName;
+         // store park code or if possible assign it as a value to the fullName
+         //if so, I can then do a separate fetch on the click that uses the parkCode to extract needed data
+         var parkCode=response.data[i].parkCode;
+         $('#park').append(`<option value="${parkCode}"> ${parkName}</option>`); 
+         console.log(parkName, parkCode);
+         //append park name to modal title
+         $('.modal-card-title').html(parkName);
+      }
+    })
+  };
+  //function to get park data from user selected park
+  var getDataPoints= function() {
+      var selectPark = document.getElementById("park");
+      var chosenPark= selectPark.value;
+    
+    
+      var url= 'https://developer.nps.gov/api/v1/parks?parkCode=' + chosenPark + '&api_key=yCwb05JC1ccmoZ4jFHTMcbiQIa3pV5MAeI22MlmG';
+      fetch(url).then(function(response) {
+          // Pass the data from the first fetch
+          return response.json(); 
+          }).then(function(response) {
+          console.log(response.data[0]);
+  
+    //// Use the park code to get the data points
+      var hourDetails = response.data[0].operatingHours[0].description;
+      var hours = response.data[0].operatingHours[0].standardHours;
+      var formattedHours = JSON.stringify(hours);
+      var description= response.data[0].description;
+      var directionsInfo= response.data[0].directionsInfo;
+      var directionsUrl= response.data[0].directionsUrl;
+      //city to plug into the weather API
+      var city= response.data[0].addresses[0].city;
+      
+      console.log(hours, hourDetails, description, directionsInfo, directionsUrl, city);
+      $('#parkHours').html(`Hours: <br> ${formattedHours} <br> ${hourDetails}`);
+      $('#description').html(`Description: <br> ${description}`);
+      $('#directions').html(`Directions: <br> ${directionsInfo} <br> <a href=${directionsUrl}>${directionsUrl}</a>`);
+      //input data into modal divs
+      //put function to display weather data inside this function
+      //handle bug for only one park in a state  
+      getCurrentWeather(city);
   })
-}
-
- var getDataPoints= function() {
-  var selectPark = document.getElementById("park");
-  var chosenPark= selectPark.value;
+      //fetch data from API for current weather using city from park api data
+      var getCurrentWeather = function(city) {
+          //console.log(1);
+          var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=046dddbb0aa4d31febc4e77558997908";
   
+          fetch(apiUrl)
+          .then(function(response) {
+              //request was successful
+              if (response.ok) {
+                  response.json().then(function(data) {
+                      //console.log(data);
+                      displayCurrentWeather(data, city);
+                  });
+              } else {
+                  alert("Error: " + response.statusText);
+              }
+          })
+      };
+      
+  };
   
-var url= 'https://developer.nps.gov/api/v1/parks?parkCode=' + chosenPark + '&api_key=yCwb05JC1ccmoZ4jFHTMcbiQIa3pV5MAeI22MlmG';
-fetch(url).then(function(response) {
-  // Pass the data from the first fetch
-  return response.json(); 
-}).then(function(response) {
-  console.log(response.data[0]);
-
-  //// Use the park code to get the data points
-  var hours = response.data[0].operatingHours;
-  var description= response.data[0].descripion;
-  var directionsInfo= response.data[0].directionsInfo;
-  var directionsUrl= response.data[0].directionsUrl;
-  //city to plug into the weather API
-  var city= response.data[0].addresses[0].city;
-
-  console.log(hours, description, directionsInfo, directionsUrl, city);
-})
-  }
+  //modal open on change of select menu
+  $("#park").change(function() {
+      $(".modal").addClass("is-active");
+      $('#park').html('');
+  
+  });
+  //modal close
+  $("#modalClose").on("click", function() {
+      $(".modal").removeClass("is-active");
+  });
+  
+  //display current conditions
+  var displayCurrentWeather = function(data) {
+      var temp = data.main.temp;
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind.speed;
+      document.querySelector("#currentConditions").innerHTML = `
+          <br>
+          Current Weather Conditions:
+          <br>
+          <p>Temperature: ${temp} °F<p>
+          <p>Humidity: ${humidity} %<p>
+          <p>Wind Speed: ${windSpeed} MPH<p>
+          `;
+  };
+    
+  
